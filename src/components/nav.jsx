4039efe185c0a0766f5e3374e303ca06fb886fee
Gustavo Carvalho <gsc13@tuta.io>
2022-08-24 21:31:29 -0300

    Add <Nav />

---
+++ src/components/nav.jsx
@@ -0,0 +1,11 @@
+import styles from "./nav.module.css"
+
+const Nav = () => {
+ return (
+  <nav className={styles.nav}>
+    <button>Menu</button>
+  </nav>
+ )
+}
+
+export default Nav;
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,9 +3,12 @@ import Image from 'next/image'
 import '../styles/globals.css'
 import styles from '../styles/Home.module.css'
 
+import Nav from '../components/nav'
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Nav />
       <Component {...pageProps} />
       <footer className={styles.footer}>
         <a
